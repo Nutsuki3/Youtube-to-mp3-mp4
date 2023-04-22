e03fcf3db1ad34215ac7ac7b3d7b5d0c760d0948
Amashiro Nutsuki <nutsukiamsr3@gmail.com>
2023-04-22 19:34:34 +0700

    Update Converting.py

--- Converting.py
+++ Converting.py
@@ -1,21 +1,36 @@
 import os
-from pytube import YouTube
+import pytube
 import moviepy.editor as mp
+from pytube.exceptions import VideoUnavailable
 
-yt_link = input("Enter the YouTube link you want to convert: ")
-yt = YouTube(yt_link)
+def Convertor(url, omp):
+    try:
+        yt = pytube.YouTube(url)
+        stream = yt.streams.filter(file_extension='mp4').first()
+        filename = stream.default_filename
+        stream.download()
+        video = mp.VideoFileClip(filename)
+        audio = video.audio    
+        if omp == "-mp3":  
+            audio.write_audiofile(os.path.splitext(filename)[0] + ".mp3")
+            print(f"Download {omp[1:]} finished!")
+            video.close()
+            audio.close()
+            os.remove(filename)
+        elif omp == "-mp4":    
+            print(f"Download {omp[1:]} finished!")
+            video.close()
+            audio.close()    
+        else:
+            audio.write_audiofile(os.path.splitext(filename)[0] + ".mp3")
+            print("Download mp3, mp4 finished!")
+            video.close()
+            audio.close()
+    except VideoUnavailable:
+        print("Videno Not Found")
 
-
-stream = yt.streams.first()
-stream.download()
-
-
-video = mp.VideoFileClip(f"{yt.title}.mp4")
-audio = video.audio
-audio.write_audiofile(f"{yt.title}.mp3")
-
-
-video.close()
-audio.close()
-os.remove(f"{yt.title}.mp4")
-
+while True:
+    info = input(">>> ")
+    omp = info[0:4]
+    url = info[5:]
+    Convertor(url, omp)
